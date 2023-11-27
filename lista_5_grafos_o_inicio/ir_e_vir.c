e850b4aae1afceaba0f0cf540c71218b0809da59
R-enanVieira <renanv.guedes7@gmail.com>
2023-11-27 12:42:54 -0300

    adding floyd warshall

--- lista_5_grafos_o_inicio/ir_e_vir.c
+++ lista_5_grafos_o_inicio/ir_e_vir.c
@@ -1,7 +1,9 @@
+#include <math.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#define maxV 2000;
+#define maxV 2001
 
 typedef struct edge {
   int v;
@@ -22,10 +24,10 @@ struct graph {
 
 typedef struct graph *Graph;
 
-static int pre[maxV];
+static int pre[maxV], cnt = 0;
 
 int **MATRIXinit(int R, int C, int x) {
-  int **m = (int **)malloc(sizeof(int) * R);
+  int **m = (int **)malloc(sizeof(int *) * R);
   for (int i = 0; i < C; i++) m[i] = (int *)malloc(sizeof(int) * C);
 
   for (int i = 0; i < R; i++)
@@ -44,40 +46,49 @@ Graph GRAPHinit(int V) {
 
 void GRAPHInsertE(Graph G, Edge E) {
   int v = E.v, w = E.w;
-  G->V++;
+  if (G->adj[v][w] == 0) G->E++;
   G->adj[v][w] = 1;
-}
-
-void GRAPHRemoveE(Graph G, Edge E) {
-  int v = E.v, w = E.w;
-  G->V++;
-  G->adj[v][w] = 0;
-}
-
-int GRAPHEdges(Edge[], Graph);
-Graph GRAPHCopy(Graph);
-void GRAPHDestroy(Graph);
-
-void dfsR(Graph G, Edge e) {
-  int t, w = e.w;
-  pre[w] = cnt++;  // variaveis globais
-  for (t = 0; t < G->V; t++) {
-    if (G->adj[w][t] != 0)
-      if (pre[t] == -1) dfsR(G, EDGE(w, t));
-  }
 }
 
 void FloydWarshall(Graph G) {
   int i, s, t;
+
   G->tc = MATRIXinit(G->V, G->V, 0);
+
   for (s = 0; s < G->V; s++)
     for (t = 0; t < G->V; t++) G->tc[s][t] = G->adj[s][t];
+
   for (s = 0; s < G->V; s++) G->tc[s][s] = 1;
+
   for (i = 0; i < G->V; i++)
-    for (s = 0; G->V; s++)
+    for (s = 0; s < G->V; s++)
       if (G->tc[s][i] == 1)
         for (t = 0; t < G->V; t++)
-          if (G->tc[i][t == 1]) G->tc[s][t] = 1;
+          if (G->tc[i][t] == 1) G->tc[s][t] = 1;
 }
 
-int main() { return 0; }
+int main() {
+  int n, m;
+
+  while (scanf("%d %d", &n, &m) == 2 && n != 0 && m != 0) {
+    Graph G = GRAPHinit(n);
+
+    for (int i = 0; i < m; i++) {
+      int v, w, p;
+      scanf("%d %d %d", &v, &w, &p);
+
+      GRAPHInsertE(G, EDGE(v - 1, w - 1));
+      if (p == 2) GRAPHInsertE(G, EDGE(w - 1, v - 1));
+    }
+
+    FloydWarshall(G);
+
+    bool ans = true;
+    for (int i = 0; i < n; i++)
+      for (int j = 0; j < n; j++) ans &= G->tc[i][j];
+
+    printf("%d\n", (ans == true ? 1 : 0));
+  }
+
+  return 0;
+}
--- lista_5_grafos_o_inicio/natureza.c
+++ lista_5_grafos_o_inicio/natureza.c
@@ -1,20 +1,23 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
 
 #define maxV 5000
 #define mod 315931
 #define colLimit 35
 
-typedef struct {int v; int w;} Edge;
+typedef struct {
+  int v;
+  int w;
+} Edge;
 Edge EDGE(int V, int W) {
   Edge e;
   e.v = V;
   e.w = W;
   return e;
 }
-typedef struct graph* Graph;
+typedef struct graph *Graph;
 
 typedef struct node {
   int v;
@@ -23,7 +26,7 @@ typedef struct node {
 
 struct graph {
   int V, E;
-  node *adj; 
+  node *adj;
 };
 
 typedef struct item {
@@ -40,7 +43,7 @@ Graph graphInit(int V) {
   Graph G = malloc(sizeof(*G));
   G->V = V;
   G->E = 0;
-  G->adj = malloc(V*sizeof(node));
+  G->adj = malloc(V * sizeof(node));
   for (v = 0; v < V; v++) {
     G->adj[v].v = 0;
     G->adj[v].next = NULL;
@@ -49,8 +52,7 @@ Graph graphInit(int V) {
 }
 
 void graphInsert(Graph G, Edge e) {
-  int v = e.v, 
-      w = e.w;
+  int v = e.v, w = e.w;
 
   node *newElement = malloc(sizeof(node));
   newElement->v = w;
@@ -63,33 +65,32 @@ void graphInsert(Graph G, Edge e) {
 int graphEdges(Edge a[], Graph G) {
   int v, E = 0;
   node *t;
-  for(v = 0; v < G->V; v++)
-     for(t = G->adj[v].next; t != NULL; t = t->next)
-      if(v < t->v) a[E++] = EDGE(v, t->v);
+  for (v = 0; v < G->V; v++)
+    for (t = G->adj[v].next; t != NULL; t = t->next)
+      if (v < t->v) a[E++] = EDGE(v, t->v);
+
+  return E;
 }
 
 void dfsR(Graph G, Edge e) {
   int t, w = e.w;
+  cnt++;
   node *l;
-  pre[w] = 1; //variaveis globais
-  for(l = G->adj[w].next; l != NULL; l = l->next) {
+  pre[w] = 1;  // variaveis globais
+  for (l = G->adj[w].next; l != NULL; l = l->next) {
     t = l->v;
-    if(!pre[t]) 
-      cnt++;
-      dfsR(G, EDGE(w, t));
+    if (!pre[t]) dfsR(G, EDGE(w, t));
   }
 }
 
 int graphSearch(Graph G) {
   int v, ans = 0;
-  for (v = 0; v < G->V; v++)
-    pre[v] = 0;
+  for (v = 0; v < G->V; v++) pre[v] = 0;
 
-  for(v = 0; v < G->V; v++) {
-    if(!pre[v]) {
-      cnt = 0;     
-      dfsR(G, EDGE(v,v));
-      printf("%d\n", cnt);
+  for (v = 0; v < G->V; v++) {
+    if (!pre[v]) {
+      cnt = 0;
+      dfsR(G, EDGE(v, v));
       ans = (cnt > ans ? cnt : ans);
     }
   }
@@ -99,73 +100,72 @@ int graphSearch(Graph G) {
 int hashU(char *v, int M) {
   int h, a = 31415, b = 27183;
   for (h = 0; *v != '\0'; v++) {
-    a = a*b%(M-1);
-    h = (a*b+*v)%M;
+    a = a * b % (M - 1);
+    h = (a * b + *v) % M;
   }
   return (h < 0 ? h + M : h);
 }
 
 bool is_equal(char *str1, char *str2) {
-    int i;
-    for(i = 0; str1[i] != 0 && str2[i] != 0; i++) {
-        if(str1[i] != str2[i])
-            return false;
-    }
+  int i;
+  for (i = 0; str1[i] != 0 && str2[i] != 0; i++) {
+    if (str1[i] != str2[i]) return false;
+  }
 
-    return str1[i] == str2[i];
+  return str1[i] == str2[i];
 }
 
-int hash_search(item *ht, char *value) {
+item *hash_search(item *ht, char *value) {
   int h = hashU(value, mod);
-  printf("hash: %d\n", h);
-  printf("search_string %s\n", value);
-  for(item *aux = ht[h].prox; aux != NULL; aux = aux->prox) {
-    printf("string: %s\n", aux->s);
-    printf("key: %d\n", aux->key);
-    if(is_equal(aux->s, value)) return aux->key;
+  item *aux;
+  aux = ht[h].prox;
+  while (aux != NULL) {
+    if (is_equal(aux->s, value)) return aux;
+    aux = aux->prox;
   }
-  return -1;
+  return NULL;
 }
 
 void hash_insert(item *ht, int key, char *value) {
   int h = hashU(value, mod);
-  printf("%d\n", h);
-  printf("%d\n", key);
   item *new;
   new = malloc(sizeof(item));
-  
+
   new->s = value;
   new->key = key;
   new->prox = ht[h].prox;
-
   ht[h].prox = new;
 }
 
 int main() {
-  int C, R; 
-  while(scanf("%d %d", &C, &R), C != 0 && R != 0) {
+  int C, R;
+  while (scanf("%d %d", &C, &R), C != 0 && R != 0) {
     Graph G = graphInit(C);
 
     item *HT;
-    HT = malloc(sizeof(item)*mod);
+    HT = malloc(sizeof(item) * mod);
     for (int i = 0; i < mod; i++) HT[i].prox = NULL;
-    
+
     for (int i = 0; i < C; i++) {
-      char s[35]; scanf("%s", s);
-      hash_insert(HT, i, s);
+      char s[35];
+      scanf(" %s", s);
+
+      char *temp = malloc(sizeof(char) * 35);
+      strcpy(temp, s);
+
+      hash_insert(HT, i, temp);
     }
 
-    char v[35], w[35];
-    while(R--) {
-      scanf("%s", v);
-      scanf("%s", w);
-      int key_v;
-      int key_w;
-      key_v = hash_search(HT, v);
-      key_w = hash_search(HT, w);
-      printf("%d %d\n", key_v, key_w);
+    char v[35];
+    char w[35];
+    for (int i = 0; i < R; i++) {
+      scanf(" %s %s", v, w);
 
-      //graphInsert(G, EDGE(hash_search(HT, v), hash_search(HT, w)));
+      int key_v = hash_search(HT, v)->key;
+      int key_w = hash_search(HT, w)->key;
+
+      graphInsert(G, EDGE(key_v, key_w));
+      graphInsert(G, EDGE(key_w, key_v));
     }
     printf("%d\n", graphSearch(G));
   }
